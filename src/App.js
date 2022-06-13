ab4f3fd5969eaad6e9994c1709a6a5bac7791311
jonnyhoudini <jmcaveety@gmail.com>
2022-06-13 22:11:06 +0100

    finished MVP and extension

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    { description: 'Do homework', priority: 'high' },
+    { description: 'Make dinner', priority: 'high' },
+    { description: 'Pick up kids', priority: 'high' },
+    { description: 'Get shopping', priority: 'low' },
+    { description: 'Call sister', priority: 'low' }
+  ]);
+
+  const [newTodo, setNewTodo] = useState("");
+  const [newPriority, setPriority] = useState("");
+
+  const handleTodoInput = (event) => {
+    setNewTodo(event.target.value);
+  }
+
+  const handlePriorityInput = (event) => {
+    setPriority(event.target.value);
+  }
+
+  const saveNewTodo = (event) => {
+    event.preventDefault();
+    const copyTodos = [...todos];
+    copyTodos.push({ description: newTodo, priority: newPriority });
+    setTodos(copyTodos);
+    setNewTodo('');
+  }
+
+  const todoNodes = todos.map((todo, index) => {
+    return (
+      <li key={index} className={todo.priority === 'high' ? "high" : "low"}><span>{todo.description}</span></li>
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>To Do List</h1>
+      <hr />
+      <ul>
+        {todoNodes}
+      </ul>
+
+      <form onSubmit={saveNewTodo}>
+        <input id='new-todo' type='text' placeholder='Add new task' value={newTodo} onChange={handleTodoInput} />
+
+        <label>High <input type='radio' value='high' id='high' name='priority' onClick={handlePriorityInput} /></label>
+        <label>Low <input type='radio' value='low' id='low' name='priority' onClick={handlePriorityInput} /></label>
+        <input type='submit' value='GO' />
+      </form>
     </div>
   );
 }
